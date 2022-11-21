5f9462c7a47ce16ecf8bcd11047475cc53600f6b
Roomcar <noteforziio@163.com>
2022-11-21 09:42:31 +0800

    debug login

--- src/views/user/user.js
+++ src/views/user/user.js
@@ -1,6 +1,6 @@
 import { createElement as vNode, Fragment, useState } from "../../../vendor/react.js";
 import { Radio, Space, Select, Button } from "../../../vendor/tdesign.min.js";
-import { register } from "../../utils/api/user.js";
+import { register, login } from "../../utils/api/user.js";
 
 export default function User(props) {
   return vNode(Fragment, null, [
@@ -9,3 +9,4 @@ export default function User(props) {
 }
 
 window.__register = register;
+window.__login = login;
