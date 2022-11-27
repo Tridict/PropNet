107efbd9a722d93b001f0cd3d43ae3e7acaa88be
Roomcar <noteforziio@163.com>
2022-11-27 17:27:27 +0800

    save draft

--- draft.js
+++ draft.js
@@ -1,5 +1,121 @@
+import { Form } from "./vendor/tdesign.min";
+
+const xx = () => {
+
+  const getSomeItems = (num, flag) => {
+    const flag_map = {
+      'random': ()=>{},
+      'fresh': ()=>{},
+      'wanted': ()=>{},
+    };
+  };
+
+};
 
 
-// 
+
+morpheme;
+form;
+token;
+format;
+
+
+
+
+entry;
+
+
+entry = {
+  form: form(),
+  meaning: meaning(),
+};
+
+
+
+
+
+
+
+
+value = {};
+label = {};
+
+model = {};
+
+type = {};
+
+slot = {};
+
+edge = {
+  // lang: "FN-Meta",
+  ref_name: "",
+  desc: "",
+  symmetric: true || false,
+  slot0: {
+    // lang: "FN-Meta",
+    ref_name: "",
+    desc: "",
+  },
+  slot1: {
+    // lang: "FN-Meta",
+    ref_name: "",
+    desc: "",
+  },
+};
+
+
+value_model = {
+  ref_name: "",
+  desc: "",
+  from_type: "int|float|bool",
+};
+dimension = {
+  ref_name: "",
+  desc: "",
+  value_model: "",
+};
+domain = {
+  ref_name: "",
+  desc: "",
+  dimensions: [
+    dimension
+  ],
+};
+
+
+
+frame = {
+  slots: [],
+  edges: [],
+};
+
+item
+
+
+
+relation = frame();
+
+prefab = {
+  frame: frame("#"),
+  elements: [
+    {
+      slot: this.frame.slot("#"),
+      content: item("#"),
+    },
+  ],
+};
+
+item = prefab | label;
+
+
+
+
+
+
+
+
+
+
+
 
 
