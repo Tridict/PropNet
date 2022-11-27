c7b02539afdfc8e64bbb6ad385bb37e11c585296
Roomcar <noteforziio@163.com>
2022-11-28 00:07:50 +0800

    save draft

--- draft.js
+++ draft.js
@@ -64,10 +64,17 @@ edge = {
 };
 
 
+value_set = {
+  uniq: true | false,
+  seq: true | false,
+  infinite: true | false,
+  enumerated: true | false,
+  samples: [],
+};
 value_model = {
   ref_name: "",
   desc: "",
-  from_type: "int|float|bool|enum",
+  from_type: "int|float|bool|fraction|complex|angle|radian|hex|byte|discrete|hash|value_set|...",
 };
 dimension = {
   ref_name: "",
@@ -84,8 +91,11 @@ domain = {
 
 
 
+
 frame = {
   // lang: "FN-Meta",
+  ref_name: "",
+  desc: "",
   slots: [
     {
       // lang: "FN-Meta",
@@ -124,10 +134,83 @@ item = prefab | label;
 
 
 
+mappingMethod = {
+  ref_name: "",
+};
+
+mappingCase = {
+  method: "",
+  input: "",
+  output: "",
+};
 
 
 
 
+
+
+
+// X 描述了 XY 的 ____
+// 特点
+// 主体
+// 来源
+// 名称
+// 类型
+// X 和 XY 的关系是 ____
+// 特点-主体
+// 整体-部分
+// 主体-产物
+// 名称
+// 类型
+
+
+
+
+// 将要被 X 时会感到 _______
+// 正在被 X 时会感到 _______
+// 刚被 X 之后会感到 _______
+// 回想起被 X 会感到 _______
+
+
+
+
+question = {
+  ref_name: "",
+  lang: "",
+
+  version: "",
+
+  title: "",
+  intro: "",
+  tips: [""],
+
+  frame: {
+    slots: [],
+  },
+  args: {
+    [slot.ref_name]: {
+      // 其实是一个 item
+      id: 00,
+      ref_name: "",
+      string: "",
+      // ...
+    },
+  },
+
+  pattern: [
+    {t: ""},  // key 是 t  取 value 的文本内容
+    {l: "_"},  // key 是 l  显示下划线
+    {a: "<slot.ref_name>"},  // key 是 a  取 value 对应 arg 的 string
+  ],
+
+  choices: [
+    {
+      label: "",
+      value: "",
+      pattern: [{t: ""}],
+    },
+  ],
+};
 
 
 
