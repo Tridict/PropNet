f66be3511cf7426f5d131c6c5e18810881a95b29
yqianjiang <y.jiang.psy@gmail.com>
2022-11-27 19:18:31 +0800

    feat: 表单使用嵌套name

--- src/views/primitives/edge.js
+++ src/views/primitives/edge.js
@@ -23,53 +23,20 @@ const { FormItem } = Form;
 
 
 export function EdgeCreatePage() {
-  const lang_of_last_created_item = storage.getItem('lang_of_last_created_item');
+  // const lang_of_last_created_item = storage.getItem('lang_of_last_created_item');
 
   const [form] = Form.useForm();
   const symmetric = Form.useWatch('symmetric', form);
 
-  useEffect(()=>{
-    form.setFieldsValue({
-      lang: lang_of_last_created_item,
-    });
-  }, []);
-
-  const [lang, setLang] = useState('');
-  const [langOptions, changeLangOptions] = useState([
-    { label: '普通话', value: 'zh-cn' },
-    { label: '美式英语', value: 'en-us' },
-    { label: '元语言', value: 'FN-Meta' },
-    { label: '(留空)', value: null },
-  ]);
-  const onChangeLang = (value) => {
-    setLang(value);
-  };
-  const onCreateLang = (value) => {
-    changeLangOptions(langOptions.concat([{ value, label: value }]));
-  };
+  // useEffect(()=>{
+  //   form.setFieldsValue({
+  //     lang: lang_of_last_created_item,
+  //   });
+  // }, []);
 
   const onSubmit = async (evt) => {
     if (evt.validateResult === true) {
-      const slot0 = form.getFieldsValue(['slot0__ref_name', 'slot0__desc']);
-      const slot1 = form.getFieldsValue(['slot1__ref_name', 'slot1__desc']);
-      const data = form.getFieldsValue(["ref_name", "desc", "symmetric"]);
-      Object.assign(data, {
-        lang: "FN-Meta",
-        slot0: {
-          lang: "FN-Meta",
-          ref_name: slot0.slot0__ref_name,
-          desc: slot0.slot0__desc,
-        },
-      })
-      if (!data.symmetric) {
-        Object.assign(data, {
-          slot1: {
-            lang: "FN-Meta",
-            ref_name: slot1.slot1__ref_name,
-            desc: slot1.slot1__desc,
-          },
-        })
-      }
+      const data = form.getFieldsValue(true);
       // storage.setItem('lang_of_last_created_item', form.getFieldValue("lang"));
       console.log(evt);
       console.log(data);
@@ -84,25 +51,57 @@ export function EdgeCreatePage() {
     MessagePlugin.info(JSON.stringify(evt));
   };
 
+  // 自定义异步校验器
+  function refnameValidator(val) {
+    return new Promise((resolve) => {
+      const timer = setTimeout(() => {
+        const data = form.getFieldsValue(true);
+        let hasSame = false;
 
-  const rules = {
-    name: [
-      { required: true, message: '必填', type: 'error' },
-    ],
-  };
+        const ref_names = {};
+        let curr = data?.ref_name;
+        if (curr != undefined) {
+          ref_names[curr] = 1;
+        }
+        curr = data?.slot0?.ref_name;
+        if (curr != undefined) {
+          if (curr in ref_names) {
+            hasSame = true;
+          }
+          ref_names[curr] = 1;
+        }
+        curr = data?.slot1?.ref_name;
+        if (curr != undefined) {
+          if (curr in ref_names) {
+            hasSame = true;
+          }
+          ref_names[curr] = 1;
+        }
+        resolve(!hasSame);
+        clearTimeout(timer);
+      });
+    });
+  }
+
 
   return vNode(Form, {
     form: form,
     colon: false,
     labelWidth: 160,
-    rules,
     onSubmit,
     onReset,
   }, [
 
     vNode(FormItem, {
+      label: 'lang',
+      name: "lang",
+      initialData: "FN-Meta",
+    }, vNode(Input, {disabled: true,})),
+
+    vNode(FormItem, {
       label: "名称",
       name: "ref_name",
+      rules: [{required: true},{validator: refnameValidator, message: 'ref_name不能重复'}],
     }, vNode(Input)),
 
     vNode(FormItem, {
@@ -113,37 +112,48 @@ export function EdgeCreatePage() {
     vNode(FormItem, {
       label: "symmetric",
       name: "symmetric",
+      initialData: false,
     }, vNode(Switch)),
 
     vNode('div', null, [
       vNode('p', null, 'slot0'),
       vNode(FormItem, {
+        label: 'lang',
+        name: ['slot0', "lang"],
+        initialData: "FN-Meta",
+      }, vNode(Input, {disabled: true,})),
+      vNode(FormItem, {
         label: "slot0 - 名称",
-        name: "slot0__ref_name",
+        rules: [{required: true},{validator: refnameValidator, message: 'ref_name不能重复'}],
+        name: ['slot0', 'ref_name'],
       }, vNode(Input)),
-  
+      
       vNode(FormItem, {
         label: "slot0 - desc",
-        name: "slot0__desc",
+        name: ['slot0', 'desc'],
       }, vNode(Input)),
     ]),
 
     symmetric ? '': vNode('div', null, [
       vNode('p', null, 'slot1'),
       vNode(FormItem, {
+        label: 'lang',
+        name: ['slot1', "lang"],
+        initialData: "FN-Meta",
+      }, vNode(Input, {disabled: true,})),
+      vNode(FormItem, {
         label: "slot1 - 名称",
-        name: "slot1__ref_name",
+        rules: [{required: true},{validator: refnameValidator, message: 'ref_name不能重复'}],
+        name: ['slot1', 'ref_name'],
       }, vNode(Input)),
   
       vNode(FormItem, {
         label: "slot1 - desc",
-        name: "slot1__desc",
+        name: ['slot1', 'desc'],
       }, vNode(Input)),
     ]),
 
-    vNode(FormItem, {
-      name: "submitButton",
-    }, vNode(Space, {
+    vNode(FormItem, null, vNode(Space, {
       direction: "horizontal",
       size: "small",
       align: "center",
